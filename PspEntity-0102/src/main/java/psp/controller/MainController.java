b1ae6896900c7e62e4b1f87094c6e71b3c62c5ee
86178 <2465670863@qq.com>
2023-10-10 22:03:47 +0800

    The rough structure of the whole project

---
+++ PspEntity-0102/src/main/java/psp/controller/MainController.java
@@ -0,0 +1,14 @@
+package psp.controller;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @RequestMapping("/test")
+    public String Test(){
+        return "test";
+    }
+}
---
+++ PspEntity-02/src/main/java/psp/PspApplication.java
@@ -0,0 +1,13 @@
+package psp;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.psp.mapper")
+public class PspApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PspApplication.class, args);
+    }
+}
